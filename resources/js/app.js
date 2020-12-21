079d4c14fafd2d849e1bf7ccc56f04bf7a9f9800
h.shimokawa <h.shimokawa@flag-pictures.co.jp>
2020-12-21 17:15:21 +0900

    update app.js and HeaderComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,10 @@ import Vue from 'vue';
 import store from './store';
 import VueRouter from 'vue-router';
 import HeaderComponent from "./components/HeaderComponent";
-import TaskListComponent from "./components/TaskListComponent";
+import LoginComponent from "./components/LoginComponent";
 import TaskCreateComponent from "./components/TaskCreateComponent";
 import TaskEditComponent from "./components/TaskEditComponent";
-import LoginComponent from "./components/LoginComponent";
+import TaskListComponent from "./components/TaskListComponent";
 import UserComponent from "./components/UserComponent";
 
 window.Vue = require('vue');
@@ -32,17 +32,20 @@ const router = new VueRouter({
         {
             path: '/',
             name: 'task.list',
-            component: TaskListComponent
+            component: TaskListComponent,
+            meta: { requiresAuth: true }
         },
         {
             path: '/tasks/create',
             name: 'task.create',
-            component: TaskCreateComponent
+            component: TaskCreateComponent,
+            meta: { requiresAuth: true }
         },
         {
             path: '/tasks/:taskId/edit',
             name: 'task.edit',
             component: TaskEditComponent,
+            meta: { requiresAuth: true },
             props: true
         },
     ]
@@ -64,28 +67,9 @@ router.beforeEach((to, from, next) => {
 });
 
 
-
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
 Vue.component('header-component', HeaderComponent);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
 });
--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -2,28 +2,26 @@
   <div class="container-fluid bg-dark mb-3">
     <div class="container">
       <nav class="navbar navbar-dark">
-        <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
+        <span class="navbar-brand mb-0 h1">todo</span>
         <div>
-          <router-link v-bind:to="{ name: 'task.list' }">
-            <button class="btn btn-success">List</button>
-          </router-link>
-          <router-link v-bind:to="{ name: 'task.create' }">
-            <button class="btn btn-success">ADD</button>
-          </router-link>
-          <router-link v-bind:to="{ name: 'login' }">
-            <button class="btn btn-success">Login</button>
-          </router-link>
-          <router-link v-bind:to="{ name: 'user' }">
-            <button class="btn btn-success">ユーザー</button>
-          </router-link>
+            <router-link v-bind:to="{ name: 'task.list' }">
+              <button class="btn btn-success">List</button>
+            </router-link>
+            <router-link v-bind:to="{ name: 'task.create' }">
+              <button class="btn btn-success">Add</button>
+            </router-link>
+            <router-link v-bind:to="{ name: 'user' }">
+              <button class="btn btn-success">User</button>
+            </router-link>
+            <router-link v-bind:to="{ name: 'login' }">
+              <button class="btn btn-success">Login</button>
+            </router-link>
             <button @click="logout" class="btn btn-success">logout</button>
-          <!-- <li @click="logout">ログアウト</li> -->
         </div>
       </nav>
     </div>
   </div>
 </template>
-
 
 <script>
     export default {
@@ -32,9 +30,9 @@
                 axios.post('api/auth/logout').then(res => {
                     axios.defaults.headers.common['Authorization'] = '';
                     state.isLogin = false;
-                    this.$router.push({path: '/'});
+                    this.$router.push({path: '/login'});
                 });
-            }
+            },
         }
     }
 </script>
