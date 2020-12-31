3691cc411a83ebb2c49f6aa3aafdaaf7180fb561
h.shimokawa <h.shimokawa@flag-pictures.co.jp>
2020-12-31 16:19:02 +0900

    clear create-form

--- resources/js/components/TaskCreateComponent.vue
+++ resources/js/components/TaskCreateComponent.vue
@@ -5,15 +5,60 @@
         <form v-on:submit.prevent="submit">
           <div class="form-group row">
             <label for="title" class="col-sm-3 col-form-label">Title</label>
-            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
+            <input
+              type="text"
+              class="col-sm-9 form-control"
+              id="title"
+              v-model="task.title"
+            />
           </div>
-          <div class="form-group row">
-            <label for="status" class="col-sm-3 col-form-label">Status</label>
-            <input type="text" class="col-sm-9 form-control" id="status" v-model="task.status">
-          </div>
+
+          <fieldset class="form-group">
+            <div class="row">
+              <legend class="col-form-label col-sm-3 pt-0">Stutas</legend>
+              <div class="col-sm-9">
+                <div class="form-check">
+                  <input
+                    type="radio"
+                    class="form-check-input"
+                    id="status0"
+                    value="0"
+                    v-model="task.status"
+                  />
+                  <label for="status0" class="form-check-label">未了</label>
+                </div>
+                <div class="form-check">
+                  <input
+                    type="radio"
+                    class="form-check-input"
+                    id="status1"
+                    value="1"
+                    v-model="task.status"
+                  />
+                  <label for="status1" class="form-check-label">進行中</label>
+                </div>
+                <div class="form-check">
+                  <input
+                    type="radio"
+                    class="form-check-input"
+                    id="status2"
+                    value="2"
+                    v-model="task.status"
+                  />
+                  <label for="status2" class="form-check-label">完了</label>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+
           <div class="form-group row">
             <label for="due" class="col-sm-3 col-form-label">Due</label>
-            <input type="" class="col-sm-9 form-control" id="due" v-model="task.due">
+            <input
+              type="date"
+              class="col-sm-9 form-control"
+              id="due"
+              v-model="task.due"
+            />
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
@@ -24,18 +69,17 @@
 
 <script>
 export default {
-    data: function () {
-        return {
-            task: {}
-        }
+  data: function () {
+    return {
+      task: {},
+    };
+  },
+  methods: {
+    submit() {
+      axios.post("/api/tasks", this.task).then((res) => {
+        this.$router.push({ name: "task.list" });
+      });
     },
-    methods: {
-        submit() {
-            axios.post('/api/tasks', this.task)
-                .then((res) => {
-                    this.$router.push({name: 'task.list'});
-                });
-        }
-    }
-}
+  },
+};
 </script>
