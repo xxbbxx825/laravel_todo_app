7358006595fb238b71be6d2ae3764d1eb6d6f3fa
h.shimokawa <h.shimokawa@flag-pictures.co.jp>
2020-12-31 11:21:48 +0900

    add feature/delete on front

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -11,7 +11,7 @@
           <th scope="col">Delete</th>
         </tr>
       </thead>
-
+      <span>Main task</span>
       <tbody v-if="task[0]">
         <tr>
           <th scope="row">{{ task[0].id }}</th>
@@ -26,11 +26,13 @@
             </router-link>
           </td>
           <td>
-            <button class="btn btn-danger" v-on:click="deleteTask(task[0].id)">Delete</button>
+            <button class="btn btn-danger" v-on:click="deleteTask(task[0].id)">
+              Delete
+            </button>
           </td>
         </tr>
       </tbody>
-
+      <span>Sub task</span>
       <tbody v-if="sub_tasks">
         <tr v-for="sub_task in sub_tasks" :key="sub_task.id">
           <th scope="row">{{ sub_task.id }}</th>
@@ -38,10 +40,15 @@
           <td>{{ sub_task.status }}</td>
           <td>{{ sub_task.due }}</td>
           <td>
-              <button class="btn btn-success">Edit</button>
+            <button class="btn btn-success">Edit</button>
           </td>
           <td>
-            <p>削除ボタン</p>
+            <button
+              class="btn btn-danger"
+              v-on:click="deleteSubTask(sub_task.id)"
+            >
+              Delete
+            </button>
           </td>
         </tr>
       </tbody>
@@ -54,38 +61,41 @@ export default {
   props: {
     taskId: {
       type: Number,
-      require: true
+      require: true,
     },
   },
   data: function () {
-      return {
-          task: {},
-          sub_tasks: {}
-      }
+    return {
+      task: {},
+      sub_tasks: {},
+      componentKey: 0,
+    };
   },
   methods: {
-      getTask() {
-          axios.get('/api/tasks/' + this.taskId)
-              .then((res) => {
-                  this.task = res.data;
-              });
-      },
-      getSubTasks() {
-          axios.get('/api/tasks/' + this.taskId + '/sub_tasks')
-              .then((res) => {
-                  this.sub_tasks = res.data;
-              });
-      },
-      deleteTask(id) {
-        axios.delete('/api/tasks/' + id)
-            .then((res) => {
-                this.$router.push({name: 'task.list'})
-            });
-    }
+    getTask() {
+      axios.get("/api/tasks/" + this.taskId).then((res) => {
+        this.task = res.data;
+      });
+    },
+    getSubTasks() {
+      axios.get("/api/tasks/" + this.taskId + "/sub_tasks").then((res) => {
+        this.sub_tasks = res.data;
+      });
+    },
+    deleteTask(id) {
+      axios.delete("/api/tasks/" + id).then((res) => {
+        this.$router.push({ name: "task.list" });
+      });
+    },
+    deleteSubTask(subTaskId) {
+      axios.delete("/api/sub_tasks/" + subTaskId).then((res) => {
+        this.getSubTasks()
+      });
+    },
   },
   mounted() {
-      this.getTask(),
-      this.getSubTasks()
-  }
+    this.getTask(),
+    this.getSubTasks();
+  },
 };
 </script>
