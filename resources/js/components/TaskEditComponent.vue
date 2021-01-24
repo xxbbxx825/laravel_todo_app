999da4525230962748c6179997ae1f60ffeaaab0
h.shimokawa <h.shimokawa@flag-pictures.co.jp>
2021-01-25 00:05:00 +0900

    TaskEditComponentを修正

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -2,18 +2,18 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <form v-on:submit.prevent="submit"  v-if="task[0]">
+        <form v-on:submit.prevent="submit"  v-if="task">
           <div class="form-group row">
             <label for="id" class="col-sm-3 col-form-label">ID</label>
-            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task[0].id">
+            <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task.id">
           </div>
           <div class="form-group row">
             <label for="title" class="col-sm-3 col-form-label">Title</label>
-            <input type="text" class="col-sm-9 form-control" id="title" v-model="task[0].title">
+            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
           </div>
           <div class="form-group row">
             <label for="status" class="col-sm-3 col-form-label">Status</label>
-            <input type="text" class="col-sm-9 form-control" id="status" v-model="task[0].status">
+            <input type="text" class="col-sm-9 form-control" id="status" v-model="task.status">
           </div>
           <div class="form-group row">
             <label for="due" class="col-sm-3 col-form-label">Due</label>
@@ -21,7 +21,7 @@
               type="date"
               class="col-sm-9 form-control"
               id="due"
-              v-model="task[0].due"
+              v-model="task.due"
             />
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
@@ -52,7 +52,7 @@ export default {
               });
       },
       submit() {
-          axios.put('/api/tasks/' + this.taskId, this.task[0])
+          axios.put('/api/tasks/' + this.taskId, this.task)
               .then((res) => {
                   this.$router.push({name: 'task.list'})
               });
